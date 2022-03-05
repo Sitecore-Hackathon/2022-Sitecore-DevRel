1d2b0ea0efd276dc3742a1cc593d2489143ecd48
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-03-05 15:41:23 -0500

    Page View Tracking Script for Send

--- src/Project/NextStore/rendering/site/components/common/Head/Head.tsx
+++ src/Project/NextStore/rendering/site/components/common/Head/Head.tsx
@@ -12,6 +12,7 @@ const Head: FC = () => {
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `!function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");` }}></script>
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `mootrack('init', 'd447fccd-3ea9-4458-8c2e-084a97e679ca');`}}></script>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `mootrack('trackPageView');`}}></script>
       </NextHead>
     </>
   )
