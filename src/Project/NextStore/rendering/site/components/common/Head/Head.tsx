b3db9b96d76fc38696aa98c5f17a8985266e56b2
Dylan Young <5227854+dylanyoung-dev@users.noreply.github.com>
2022-03-05 11:37:11 -0500

    Inject Sitecore Send Init Script

--- src/Project/NextStore/rendering/site/components/common/Head/Head.tsx
+++ src/Project/NextStore/rendering/site/components/common/Head/Head.tsx
@@ -10,6 +10,8 @@ const Head: FC = () => {
       <NextHead>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `!function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");` }}></script>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `mootrack('init', '65f2b2b7-f5cd-43a4-9a40-1d7ef935011f');`}}></script>
       </NextHead>
     </>
   )
